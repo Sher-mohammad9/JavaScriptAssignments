3da5e647a94178a44ee1097e7a46404bfe5bdeb0
sher-mohammad9 <hassan.deshwali0@gmail.com>
2022-12-21 15:41:20 +0530

    JavaScript Assignments

--- Coaching_Question/coachingQ.js
+++ Coaching_Question/coachingQ.js
@@ -1,6 +1,16 @@
 "use strict"
 
-// Q1
+// Q1 ek number lena hai. 
+//     -> kya number odd hai ? 
+//     -> agar odd hai to kya 7 ka multiple hai ?
+//     -> Kya number me 15 add krne pr vah number 120 ke brabar hai 
+
+// Yadi teeno condition true hai to ek date ka object lena hai vah us date object me jo number
+// lia hai us number date ki minutes me add krke time print krvana hai 
+
+// Else conditon false hai to tume aaj ki date ko milliseconds me console me print krvana hai
+
+
 let num = 85;
 if(num % 2 != 0 && num % 7 === 0){
            num += 15
@@ -13,7 +23,14 @@ if(num % 2 != 0 && num % 7 === 0){
        console.log(Date.UTC(num))
 }
 
-// Q2
+// Q2 ek number lena hia yadi vah number even hai to 100 print krna hai 
+// otherwise vhi number print krna hai 
+
+// But is solution ko 3 tarike se krna hai 
+//     if else 
+//     ternary operator
+//     || operator Short circuiting
+
 let num1 = 30;
 let num2 = 100
 if(num1 % 2 === 0){
@@ -35,7 +52,14 @@ function hunderd(num1){
 }
 
 
-//Q3 
+//Q3 value leni hai country population
+
+// agar vah population 1.3 crore se jyada hai to China print krna hia 
+// agar 1.3 se kam and 1 crore se jyada to India print krna hai 
+// else 
+//     Pakistan
+
+// is question ko switch case se krna hai 
 
 let population = 1.3;
 if(population > 1.3){
@@ -59,7 +83,13 @@ switch(desigain){
 	break; 
 }
 
-// Q4
+// Q4 My Name is Sajid Ali Khan. Zahid bhai jyada bolte hain. Inko chup krana pdega
+
+// Is string me Sajid ko Zahid se and Zahid ko Sajid se replace krna hai. aur chup ko silent 
+// se replace krna hai. Iske alava jitne b words hai is string me vo sb words ka phle character 
+// Capital hona chahiye. Finally tume converted String print krvani hai
+
+// OUTPUT =  My Name Is Zahid Ali Khan. Sajid Bhai Jyada Bolte Hain. Inko Chup Krana Pdega
 
 let str = "my mame is sajid ali khan. zahid bhai jyada bolte hain. Inko chup krana pdega"
 let arr = str.split(" ")
@@ -80,7 +110,11 @@ for(let val of arr){
 }
 console.log(arr1.join(" "))
 
-// Q5 
+// Q5 9581894461
+
+// is number ko convert krna hai 
+
+// ***189xxxx
 
 let moNum = "9881894461";
 let sub = moNum.substring(3,6)
@@ -89,7 +123,17 @@ let padE = pad.padEnd(pad.length + moNum.slice(6).length, "x")
 console.log(padE)
 
 
-// Q6 
+// Q6 Ye pattern print krna hai.
+// *****
+// **** 
+// ***
+// **
+// *
+// **
+// ***
+// ****
+// *****
+
 for(let i=1; i<=5; i++){
 	for(let j=i; j<=5; j++){
 		document.write("*")
@@ -103,7 +147,13 @@ for(let i=2; i<=5; i++){
 		document.write("<br>")
 	}
 
-// Q7 
+
+// Q7 [1,2,3,4,5,6,7] answer 65
+    // har number ko 10 se multiply krna hai 
+    // check krna hia kaunsa number 50 se bda hai 
+    // jo b numbers bachenge unka sum krna hia 
+    // sare numbers ka sum krne ke bad un numbers ka average btana hai
+    // answer =  65
 
 let arr2 = [1,2,3,4,5,6,7];
 let sum = 0;
@@ -119,7 +169,16 @@ let arr3 = arr2.forEach(function(val){
 console.log(sum)
 console.log(ave/2)
 
-// Q8 
+// Q8  DOM 
+
+// HTML page tume counter chlana hai 
+// 100
+// 99
+// 98
+// 97
+
+// 2 buttons honge Start and Stop jaise hi number 0 ho jaye ek alert dhikana hia Game Over
+
 
 let second = document.getElementById("seconds")
 let sec = 10;
@@ -154,7 +213,15 @@ function start(){
 },1000)
 }
 
-// Q9 
+// Q9  Ek object1 bnana hai person details ka and iski deep copy krni hai 
+
+// String me convert krna hai. fir Dubara se Object bnana hai Object2
+
+// if object1 === object2 
+// Yes/No print krvana hai 
+
+// Object2 se object destructring krke name, mobile and pincode print krvana hai 
+ 
 
 let person1 = {
 	myName : "Hassan",
@@ -185,7 +252,14 @@ if(person1 === person2){
 let {myName,mobile,address:{pincode}} = person2;
 console.log(myName,mobile,pincode)
 
-// Q10
+
+// Q10 [1,2,3,4,1,2,3,1,2,3,1,5,6,7,4]
+
+// Tume ye btana hai ki kaunsa number kitni bar repeat hua hai aur kaunsa number maximum bar repeat hua hia 
+// 1:5
+// 2:4
+
+// OUTPUT = Number 1 total 10 times repeat hua hai
 
 let arr4 = [1,2,3,4,1,2,3,1,2,3,1,5,6,7,4];
 let map = new Map();
